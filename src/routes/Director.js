5ca17f8449f31bf4f3c5cb9ac623e723466b60a5
8790fahad <8790fahadado@gmail.com>
2020-09-09 15:40:31 +0100

    cahange

--- src/routes/Director.js
+++ src/routes/Director.js
@@ -21,7 +21,8 @@ import {
   getMailTable,
   getImagesURL,
   getImageRemark,
-  createUser
+  createUser,
+  getDepartment_Position
 } from '../controllers/Director';
 
 module.exports = (app) => {
@@ -29,6 +30,7 @@ module.exports = (app) => {
   app.get('/api/use/rate/cat', getUseRateCat);
   app.get('/api/get/review/range', getReviewRange);
   app.post('/api/rate/charge', createRateCharge);
+  app.get('/api/get/department/position',getDepartment_Position)
   app.post('/api/site_file/create', createSiteFile);
   app.post('/api/letter_template/create', createLetterTemplate);
   app.post('/api/departmentunit/create', createDepartmentunit);
