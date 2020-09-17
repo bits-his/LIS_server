592c5378699826aa0810d699a2ea7828bb071a48
sammiepius <sammiepius10@gmail.com>
2020-09-17 10:41:47 +0100

    changes

--- src/routes/Director.js
+++ src/routes/Director.js
@@ -23,7 +23,8 @@ import {
   getImageRemark,
   createUser,
   getDepartment_Position,
-  getPostion
+  getPostion,
+  createApplication,
 } from '../controllers/Director';
 
 module.exports = (app) => {
@@ -31,25 +32,26 @@ module.exports = (app) => {
   app.get('/api/use/rate/cat', getUseRateCat);
   app.get('/api/get/review/range', getReviewRange);
   app.post('/api/rate/charge', createRateCharge);
-  app.get('/api/get/department/position',getDepartment_Position)
-  app.get('/api/get/position',getPostion)
+  app.get('/api/get/department/position', getDepartment_Position);
+  app.get('/api/get/position', getPostion);
   app.post('/api/site_file/create', createSiteFile);
   app.post('/api/letter_template/create', createLetterTemplate);
   app.post('/api/departmentunit/create', createDepartmentunit);
   app.post('/api/department/create', createDepartment);
   app.post('/api/directors/create', createDirectors);
-  app.post('/api/create/user',createUser);
+  app.post('/api/application/create', createApplication);
+  app.post('/api/create/user', createUser);
   app.get('/api/get/department', getDepartment);
   app.get('/api/get/registry', getRegistry);
-  app.get('/api/get/mail/table',getMailTable)
+  app.get('/api/get/mail/table', getMailTable);
   app.get('/api/get/remarks/:tag_no', getRemarks);
-  app.get('/api/get/images/URL/:id',getImagesURL)
-  app.get('/api/get/imageRemark/:id',getImageRemark)
+  app.get('/api/get/images/URL/:id', getImagesURL);
+  app.get('/api/get/imageRemark/:id', getImageRemark);
   app.get('/api/get/department_unit', getDepartmentUnit);
   app.get('/api/generated_id', generatedId);
   app.post('/api/update/registry', updateRegistry);
-  app.get('/api/get/mail/badge',getMailBadge);
-  app.get('/api/get/letter/template/name',getLetterTemplateName);
-  app.get('/api/get/letter/body/:letter',getLetterBody)
-  app.get('/api/get/groundrent/:land/:range',getGroundRent)
+  app.get('/api/get/mail/badge', getMailBadge);
+  app.get('/api/get/letter/template/name', getLetterTemplateName);
+  app.get('/api/get/letter/body/:letter', getLetterBody);
+  app.get('/api/get/groundrent/:land/:range', getGroundRent);
 };
