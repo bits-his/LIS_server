e5477632600f5ac2ba7139b53d074ae212679b68
ibagwai9 <ibagwai9@gmail.com>
2021-08-05 15:56:30 +0100

    deploy

--- build/index.js
+++ build/index.js
@@ -26,7 +26,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // const cloudinary = require('cloudinary');
 var _require = require("./util/Cloudinary"),
-    cloudinary = _require.cloudinary;
+    cloudRoute = _require.cloudRoute;
 
 var app = (0, _express2.default)();
 
@@ -48,97 +48,18 @@ _models2.default.sequelize.sync().then(function () {
 
 // passport middleware
 app.use(_passport2.default.initialize());
+// import other routes
+require("./routes/GIS.js")(app);
+require("./routes/user.js")(app);
+require("./routes/Director.js")(app);
+// require("./routes/PostGIS.js")(app);
+cloudRoute(app);
 
 // passport config
 require("./config/passport")(_passport2.default);
-
-//default route
-app.get("/", function (req, res) {
-  return res.send("Hello my World");
-});
-require("./routes/Director.js")(app);
-require("./routes/user.js")(app);
-app.post("/api/image/upload", _multer.profileStorage.array("image"), function (req, res) {
-  var data = req.files;
-  var _req$body = req.body,
-      today = _req$body.today,
-      form_no = _req$body.form_no,
-      application_type = _req$body.application_type,
-      application_name = _req$body.application_name,
-      amount = _req$body.amount,
-      address = _req$body.address,
-      phone = _req$body.phone,
-      other_Info = _req$body.other_Info,
-      tp_no = _req$body.tp_no,
-      land_no = _req$body.land_no,
-      amount_paid = _req$body.amount_paid,
-      reciept_no = _req$body.reciept_no,
-      application = _req$body.application,
-      email = _req$body.email,
-      generate = _req$body.generate,
-      forward_by = _req$body.forward_by;
-
-  console.log(req.body);
-  _models2.default.sequelize.query("INSERT INTO application_form(application_date,form_no,type,name,amount,address,phone,other_info,tp_no,file_no,amount_paid,reciept_no,status,forward_to,email,commissioning,forward_by) VALUES\n       ('" + today + "','" + form_no + "','" + application_type + "','" + application_name + "','" + amount + "','" + address + "','" + phone + "','" + other_Info + "','" + tp_no + "','" + land_no + "','" + amount_paid + "','" + reciept_no + "','" + application + "','Director Land','" + email + "','" + generate + "','" + forward_by + "')").then(function () {
-    if (data) {
-      data.forEach(function (item) {
-        console.log(data);
-        var arr = [];
-
-        arr.push(item.path);
-        arr.push(req.body.form_no);
-        console.log(arr);
-        _models2.default.sequelize.query("INSERT INTO images(id, image_url) VALUES ('" + arr[0] + "')");
-      });
-    }
-  }).then(function (result) {
-    _models2.default.sequelize.query("UPDATE INTO application_form SET image_id=" + result["id"] + "')").then().catch(function (err) {
-      return res.json({ success: false, err: err });
-    });
-
-    if (raresultw) res.json({ success: true, raw: raw });
-  }).catch(function (err) {
-    return res.json({ success: false, msg: err });
-  });
-});
-
-app.post("/api/letter/images/upload", _multer.uploadLetter.array("image"), function (req, res) {
-  _models2.default.sequelize.query("INSERT INTO letter_of_stakeholder(select_letter_template, selectedcc, remarks) \n        VALUES ('" + req.body.selectLetter + "','" + req.body.selectCC + "','" + req.body.remarks + "')").then(function () {
-    var data = req.files;
-    if (req.files) {
-      data.forEach(function (item) {
-        var arr = [];
-        arr.push(item.path);
-        _models2.default.sequelize.query("INSERT INTO images( image_url) VALUES (\"" + arr + "\")");
-      });
-    }
-  }).then(function (results) {
-    return res.json({ success: true, results: results });
-  }).catch(function (err) {
-    return res.status(500).json({ err: err });
-  });
-});
-
-app.post("/api/surveyor/images/upload", _multer.surveyor.array("image"), function (req, res) {
-  var data = req.files;
-  _models2.default.sequelize.query("INSERT INTO surveyor_report(select_letter_template, selectedcc, remarks) VALUES (\"" + req.body.selectFile + "\",\"" + req.body.selectTemplate + "\",\"" + req.body.remarks + "\")").then(function () {
-    if (data) {
-      data.forEach(function (item) {
-        var arr = [];
-        arr.push(item.path);
-        _models2.default.sequelize.query("INSERT INTO images( image_url) VALUES (\"" + arr + "\")");
-      });
-    }
-  }).then(function (results) {
-    return res.json({ success: true, results: results });
-  }).catch(function (err) {
-    return res.status(500).json({ err: err });
-  });
-});
-
+//create a server
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log("App listening at http://%s:%s", host, port);
 });
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import models from "./models";
 import { profileStorage, uploadLetter, surveyor } from "../config/multer";
 import db from "./models";
 // const cloudinary = require('cloudinary');
-const { cloudinary } = require("./util/Cloudinary");
+const { cloudRoute } = require("./util/Cloudinary");
 
 const app = express();
 
@@ -28,116 +28,18 @@ models.sequelize.sync().then(() => {
 
 // passport middleware
 app.use(passport.initialize());
+// import other routes
+require("./routes/GIS.js")(app);
+require("./routes/user.js")(app);
+require("./routes/Director.js")(app);
+// require("./routes/PostGIS.js")(app);
+cloudRoute(app);
 
 // passport config
 require("./config/passport")(passport);
-
-//default route
-app.get("/", (req, res) => res.send("Hello my World"));
-require("./routes/Director.js")(app);
-require("./routes/user.js")(app);
-app.post("/api/image/upload", profileStorage.array("image"), (req, res) => {
-  const data = req.files;
-  const {
-    today,
-    form_no,
-    application_type,
-    application_name,
-    amount,
-    address,
-    phone,
-    other_Info,
-    tp_no,
-    land_no,
-    amount_paid,
-    reciept_no,
-    application,
-    email,
-    generate,
-    forward_by,
-  } = req.body;
-  console.log(req.body);
-  db.sequelize
-    .query(
-      `INSERT INTO application_form(application_date,form_no,type,name,amount,address,phone,other_info,tp_no,file_no,amount_paid,reciept_no,status,forward_to,email,commissioning,forward_by) VALUES
-       ('${today}','${form_no}','${application_type}','${application_name}','${amount}','${address}','${phone}','${other_Info}','${tp_no}','${land_no}','${amount_paid}','${reciept_no}','${application}','Director Land','${email}','${generate}','${forward_by}')`
-    )
-    .then(() => {
-      if (data) {
-        data.forEach((item) => {
-          console.log(data);
-          let arr = [];
-
-          arr.push(item.path);
-          arr.push(req.body.form_no);
-          console.log(arr);
-          db.sequelize.query(
-            `INSERT INTO images(id, image_url) VALUES ('${arr[0]}')`
-          );
-        });
-      }
-    })
-    .then((result) => {
-      db.sequelize
-        .query(`UPDATE INTO application_form SET image_id=${result["id"]}')`)
-        .then()
-        .catch((err) => res.json({ success: false, err }));
-
-      if (raresultw) res.json({ success: true, raw });
-    })
-    .catch((err) => res.json({ success: false, msg: err }));
-});
-
-app.post(
-  "/api/letter/images/upload",
-  uploadLetter.array("image"),
-  (req, res) => {
-    db.sequelize
-      .query(
-        `INSERT INTO letter_of_stakeholder(select_letter_template, selectedcc, remarks) 
-        VALUES ('${req.body.selectLetter}','${req.body.selectCC}','${req.body.remarks}')`
-      )
-      .then(() => {
-        const data = req.files;
-        if (req.files) {
-          data.forEach((item) => {
-            let arr = [];
-            arr.push(item.path);
-            db.sequelize.query(
-              `INSERT INTO images( image_url) VALUES ("${arr}")`
-            );
-          });
-        }
-      })
-      .then((results) => res.json({ success: true, results }))
-      .catch((err) => res.status(500).json({ err }));
-  }
-);
-
-app.post("/api/surveyor/images/upload", surveyor.array("image"), (req, res) => {
-  const data = req.files;
-  db.sequelize
-    .query(
-      `INSERT INTO surveyor_report(select_letter_template, selectedcc, remarks) VALUES ("${req.body.selectFile}","${req.body.selectTemplate}","${req.body.remarks}")`
-    )
-    .then(() => {
-      if (data) {
-        data.forEach((item) => {
-          let arr = [];
-          arr.push(item.path);
-          db.sequelize.query(
-            `INSERT INTO images( image_url) VALUES ("${arr}")`
-          );
-        });
-      }
-    })
-    .then((results) => res.json({ success: true, results }))
-    .catch((err) => res.status(500).json({ err }));
-});
-
+//create a server
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
   console.log("App listening at http://%s:%s", host, port);
 });
